5233fe2b6dd6c0dc49b5858ff0e0ebf5a6252a4e
Nikolas Grottendieck <git@nikolasgrottendieck.com>
2025-04-25 15:46:58 +0200

    feat: add ble.sh & terraform-backend-git formulae

---
+++ Formula/b/blesh.rb
@@ -0,0 +1,41 @@
+class Blesh < Formula
+  desc "Bash Line Editor―a line editor written in pure Bash with syntax highlighting, auto suggestions, vim modes, etc. for Bash interactive sessions."
+  homepage "https://github.com/akinomyoga/ble.sh"
+  url "https://github.com/akinomyoga/ble.sh/archive/refs/tags/v0.4.0-devel3.tar.gz"
+  sha256 "867ec9681bd75de7faa93622bcae1b705f561c11d83a62ead1f14554b87630dc"
+  license "BSD-3-Clause"
+  head "https://github.com/akinomyoga/ble.sh.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  def caveats
+    s = <<-EOS.undent
+      # Add this lines at the top of .bashrc:
+      [[ $- == *i* ]] && source /path/to/blesh/ble.sh --noattach
+
+      # your bashrc settings come here...
+
+      # Add this line at the end of .bashrc:
+      [[ ! ${BLE_VERSION-} ]] || ble-attach
+    EOS
+    s
+  end
+
+  depends_on "make"
+
+  option "without-docs", "Disable documentation files"
+
+  def install
+    args = []
+    args << "INSDIR_DOC=no" if build.with? "without-docs"
+
+    system "make" "install", "INSDIR=#{share}", *args
+  end
+
+  test do
+    assert_path_exists "#{share}/ble.sh"
+  end
+end
---
+++ Formula/t/terraform-backend-git.rb
@@ -0,0 +1,24 @@
+class TerraformBackendGit < Formula
+  desc "Terraform HTTP Backend implementation that uses Git repository as storage"
+  homepage "https://github.com/plumber-cd/terraform-backend-git"
+  url "https://github.com/plumber-cd/terraform-backend-git/archive/refs/tags/v0.1.8.tar.gz"
+  sha256 "96499b4d98cd2337e96f256e79b91f6249f0d57374962296109bf4baae76cd7a"
+  license "Apache-2.0"
+  head "https://github.com/plumber-cd/terraform-backend-git.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-mod=vendor",
+           *std_go_args(ldflags: "X github.com/plumber-cd/terraform-backend-git/cmd.Version=#{version}")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/terraform-backend-git --version 2>&1")
+  end
+end
