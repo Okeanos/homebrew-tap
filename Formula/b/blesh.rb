3348d76334a8ed0c17a7db951435a09eeff2a4a1
Nikolas Grottendieck <git@nikolasgrottendieck.com>
2025-04-26 10:51:45 +0200

    feat: improve blesh formula
    - improved formatting
    - make it keg only because users have to set it up manually anyway
    - fix issue with caveat message
    - move installation directory
    - add additional test

--- Formula/b/blesh.rb
+++ Formula/b/blesh.rb
@@ -1,7 +1,9 @@
 class Blesh < Formula
   desc "Bash Line Editor―a line editor written in pure Bash with syntax highlighting, auto suggestions, vim modes, etc. for Bash interactive sessions."
   homepage "https://github.com/akinomyoga/ble.sh"
-  url "https://github.com/akinomyoga/ble.sh.git", tag: "v0.4.0-devel3", revision: "1a5c451c8baa71439a6be4ea0f92750de35a7620"
+  url "https://github.com/akinomyoga/ble.sh.git",
+      tag: "v0.4.0-devel3",
+      revision: "1a5c451c8baa71439a6be4ea0f92750de35a7620"
   license "BSD-3-Clause"
   head "https://github.com/akinomyoga/ble.sh.git", branch: "master"
 
@@ -10,17 +12,18 @@ class Blesh < Formula
     strategy :github_latest
   end
 
+  keg_only "ble.ssh needs to be manually setup via .bashrc inclusion. It is useless on the PATH."
+
   def caveats
-    s = <<-EOS.undent
-      # Add this lines at the top of .bashrc:
-      [[ $- == *i* ]] && source /path/to/blesh/ble.sh --noattach
+    <<~EOS
+      # Add this line at the top of your .bashrc:
+      [[ $- == *i* ]] && source #{bin}/blesh/ble.sh --noattach
 
       # your bashrc settings come here...
 
       # Add this line at the end of .bashrc:
       [[ ! ${BLE_VERSION-} ]] || ble-attach
     EOS
-    s
   end
 
   depends_on "make"
@@ -32,10 +35,11 @@ class Blesh < Formula
     args = []
     args << "INSDIR_DOC=no" if build.with? "without-docs"
 
-    system "make", "install", "INSDIR=#{share}", *args
+    system "make", "install", "INSDIR=#{bin}", *args
   end
 
   test do
-    assert_path_exists "#{share}/ble.sh"
+    assert_path_exists "#{bin}/ble.sh"
+    assert_match version.to_s, shell_output("bash -c #{bin}/ble-sh --version 2>&1")
   end
 end
